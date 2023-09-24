fb1b1ecf24b44e0dbc89af891037c38981e877f8
Ritik <flipit001@gmail.com>
2023-09-24 13:24:30 -0700

    made pieces and rotation of pieces

--- tetris.py
+++ tetris.py
@@ -16,10 +16,37 @@ class TetrisBoard:
                 self.arr[i + t_left[0]][j + t_left[1]] = displace[i][j]
 
 
+class Piece:
+    def __init__(self, piece):
+        self.piece = piece
+
+    def __str__(self):
+        output = ""
+        for r in self.piece:
+            for c in r:
+                output += f"{c} "
+            output += "\n"
+        return output
+
+    def rotate(self):
+        N = len(self.piece)
+        for x in range(0, int(N / 2)):
+            for y in range(x, N - x - 1):
+                t = self.piece[x][y]
+
+                # move values from right to top
+                self.piece[x][y] = self.piece[y][N - 1 - x]
+
+                self.piece[y][N - 1 - x] = self.piece[N - 1 - x][N - 1 - y]
+
+                self.piece[N - 1 - x][N - 1 - y] = self.piece[N - 1 - y][x]
+                self.piece[N - 1 - y][x] = t
+
+
 if __name__ == "__main__":
     board = TetrisBoard()
     print(board)
-    displace = [[0, 0, 0, 1], [1, 0, 0, 0]]
-    board.displace((0, 3), displace)
-    print("\n" * 4)
-    print(board)
+    displace = Piece([[0, 0, 1, 1], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    print(displace)
+    displace.rotate()
+    print(displace)
