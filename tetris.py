f1723656462845e422e35273597a1943b9f49abf
Ritik <flipit001@gmail.com>
2023-09-24 17:18:23 -0700

    made pieces and rotation of pieces ACTUALLY WORK

--- tetris.py
+++ tetris.py
@@ -1,3 +1,75 @@
+# CONTSTANTS
+"""
+PIECES (for my refence)
+#####
+0 1 1
+1 1 0
+-----
+0 1
+1 1
+1 0
+----
+1 0
+1 1
+0 1
+----
+1 1 0
+0 1 1
+#####
+1 1 
+1 1
+#####
+0 1 0
+1 1 1
+-----
+1 0
+1 1
+1 0
+-----
+1 1 1
+0 1 0
+-----
+0 1
+1 1
+0 1
+#####
+1 1 1 1
+-----
+1
+1
+1
+1
+"""
+PIECES = [
+    [[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+    [[1, 1], [1, 1]],
+    [[0, 1, 0], [1, 1, 1], [0, 0, 0]],
+    [
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ],
+]
+
+
+def displace_arr(arr: list[list[int]], t_left: tuple, displace: list[list[int]]):
+    carr = [[0 for _ in arr[0]] for _ in arr]
+    for i in range(len(displace)):
+        for j in range(len(displace[i])):
+            carr[i + t_left[0]][j + t_left[1]] = displace[i][j]
+
+    return carr
+
+
+def merge_list(l1, l2):  # l1 and l2 should be the same size
+    output = [[0 for _ in l1[0]] for _ in l1]
+    for i in range(len(l1)):
+        for j in range(len(l1[i])):
+            output[i][j] = l1[i][j] | l2[i][j]
+    return output
+
+
 class TetrisBoard:
     def __init__(self, rows=20, cols=10):
         self.arr = [[0 for _ in range(cols)] for _ in range(rows)]
@@ -11,14 +83,14 @@ class TetrisBoard:
         return output
 
     def displace(self, t_left: tuple, displace: list[list[int]]):
-        for i in range(len(displace)):
-            for j in range(len(displace[i])):
-                self.arr[i + t_left[0]][j + t_left[1]] = displace[i][j]
+        self.arr = merge_list(self.arr, displace_arr(self.arr, t_left, displace))
 
 
 class Piece:
-    def __init__(self, piece):
+    def __init__(self, piece, cur_pos):
         self.piece = piece
+        self.undo = [[0 for _ in range(len(piece[0]))] for _ in range(len(piece))]
+        self.cur_pos = cur_pos
 
     def __str__(self):
         output = ""
@@ -30,23 +102,24 @@ class Piece:
 
     def rotate(self):
         N = len(self.piece)
-        for x in range(0, int(N / 2)):
-            for y in range(x, N - x - 1):
-                t = self.piece[x][y]
+        for i in range(N // 2):
+            for j in range(i, N - i - 1):
+                temp = self.piece[i][j]
+                self.piece[i][j] = self.piece[N - 1 - j][i]
+                self.piece[N - 1 - j][i] = self.piece[N - 1 - i][N - 1 - j]
+                self.piece[N - 1 - i][N - 1 - j] = self.piece[j][N - 1 - i]
+                self.piece[j][N - 1 - i] = temp
+        self.undo = [
+            [0 for _ in range(len(self.piece[0]))] for _ in range(len(self.piece))
+        ]
 
-                # move values from right to top
-                self.piece[x][y] = self.piece[y][N - 1 - x]
 
-                self.piece[y][N - 1 - x] = self.piece[N - 1 - x][N - 1 - y]
-
-                self.piece[N - 1 - x][N - 1 - y] = self.piece[N - 1 - y][x]
-                self.piece[N - 1 - y][x] = t
-
-
-if __name__ == "__main__":
-    board = TetrisBoard()
-    print(board)
-    displace = Piece([[0, 0, 1, 1], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-    print(displace)
-    displace.rotate()
-    print(displace)
+board = TetrisBoard()
+print(board)
+displace = Piece(PIECES[2], (0, 3))
+print(displace)
+displace.rotate()
+print(displace)
+board.displace(displace.cur_pos, displace.piece)
+board.displace((3, 4), displace.piece)
+print(board)
